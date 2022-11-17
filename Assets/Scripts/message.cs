a412ee2430ad1c9a8c11793fbf9f389af58e6d85
NADŽADY Matúš <matus.nadzady@asseco-ce.com>
2022-11-17 15:51:30 +0100

    Implemented async messages

--- Assets/Scripts/message.cs
+++ Assets/Scripts/message.cs
@@ -6,13 +6,19 @@ public class message : MonoBehaviour
 {
 	public Transform fromLifeline;
 	public Transform toLifeline;
-	
+
 	private LineRenderer lr;
+    private RectTransform m_RectTransform;
 
     // Start is called before the first frame update
     void Start()
     {
         lr = GetComponent<LineRenderer>();
+
+        m_RectTransform = GetComponent<RectTransform>();
+        m_RectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+        m_RectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+        m_RectTransform.pivot = new Vector2(0.5f, 0.5f);
     }
 
     // Update is called once per frame
@@ -20,11 +26,11 @@ public class message : MonoBehaviour
     {
     	Vector3 coordsFrom = fromLifeline.localPosition;
     	Vector3 coordsTo = toLifeline.localPosition;
-    	
+
     	// text component positioning
     	float x = (coordsTo.x + coordsFrom.x) / 2;
         transform.localPosition = new Vector3(x, transform.localPosition.y, transform.localPosition.z);
-        
+
         // line renderer positioning
       	float length = coordsTo.x - coordsFrom.x;
         lr.SetPosition(0, new Vector3(-length/2, -15, transform.localPosition.z));
