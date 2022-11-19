1565f5e142de9871417a3cb6d5bbe3616f8161ec
RicsiToth <richard99.toth@gmail.com>
2022-11-19 22:11:00 +0100

    Implemented synchronous arrows

--- Assets/Scripts/message.cs
+++ Assets/Scripts/message.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI.Extensions;
 
 public class message : MonoBehaviour
 {
 	public Transform fromLifeline;
 	public Transform toLifeline;
 
-	private LineRenderer lr;
+    private UILineRenderer lr;
+    private Transform text;
 
     void Start()
     {
-        lr = GetComponent<LineRenderer>();
+        lr = GetComponent<UILineRenderer>();
+        text = transform.GetChild(0);
     }
+
+    //It is messy... This draws the arrow with lines... Documentation is bad could not find how to fill out stuff
+    void CreateArrow(List<Vector2> pointList, bool toLeft, float x)
+    {
+        if (toLeft)
+        {
+            pointList.Add(new Vector2(x - 50.5f, -15));
+            pointList.Add(new Vector2(x - 60, -10));
+            pointList.Add(new Vector2(x - 60, -20));
+            pointList.Add(new Vector2(x - 50.5f, -15));
+            pointList.Add(new Vector2(x - 59, -12));
+            pointList.Add(new Vector2(x - 59, -18));
+            pointList.Add(new Vector2(x - 50.5f, -15));
+            pointList.Add(new Vector2(x - 60, -14));
+            pointList.Add(new Vector2(x - 55, -18));
+        } else
+        {
+            pointList.Add(new Vector2(x - 48.5f, -15));
+            pointList.Add(new Vector2(x - 40, -10));
+            pointList.Add(new Vector2(x - 40, -20));
+            pointList.Add(new Vector2(x - 48.5f, -15));
+            pointList.Add(new Vector2(x - 41, -12));
+            pointList.Add(new Vector2(x - 41, -18));
+            pointList.Add(new Vector2(x - 48.5f, -15));
+            pointList.Add(new Vector2(x - 40, -14));
+            pointList.Add(new Vector2(x - 45, -18));
+        }
+    }
+
+    void CreateLine(List<Vector2> pointList, bool toLeft, float from, float to)
+    {
+        if (toLeft)
+        {
+            pointList.Add(new Vector2(from - 49, -15));
+            pointList.Add(new Vector2(to - 50, -15));
+        }
+        else
+        {
+            pointList.Add(new Vector2(from - 50, -15));
+            pointList.Add(new Vector2(to - 49, -15));
+        }
+    }
+
 
     void Update()
     {
@@ -21,11 +67,14 @@ public class message : MonoBehaviour
 
     	// text component positioning
     	float x = (coordsTo.x + coordsFrom.x) / 2;
-        transform.localPosition = new Vector3(x, transform.localPosition.y, transform.localPosition.z);
+        text.localPosition = new Vector3(x - 50, transform.localPosition.y - 25, transform.localPosition.z);
 
         // line renderer positioning
       	float length = coordsTo.x - coordsFrom.x;
-        lr.SetPosition(0, new Vector3(-length/2, -15, transform.localPosition.z));
-        lr.SetPosition(1, new Vector3(length/2, -15, transform.localPosition.z));
+        bool toLeft = coordsTo.x > coordsFrom.x;
+        List<Vector2> pointList = new List<Vector2>();
+        CreateLine(pointList, toLeft, coordsFrom.x, coordsTo.x);
+        CreateArrow(pointList, toLeft, coordsTo.x);
+        lr.Points = pointList.ToArray(); 
     }
 }
