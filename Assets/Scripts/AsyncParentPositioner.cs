883419166dcd7d85a631b0921ad063c36cbe58d1
Andrej Vasek <vasek.andrej@gmail.com>
2022-12-22 17:56:58 +0100

    fix lifeline and async parent positioning on non default resolutions

--- Assets/Scripts/AsyncParentPositioner.cs
+++ Assets/Scripts/AsyncParentPositioner.cs
@@ -7,7 +7,7 @@ public class AsyncParentPositioner : MonoBehaviour
 
     private int numOfChilds;
     private message[] messagesList;
-    private Vector3 newPosition;
+    // private Vector3 newPosition;
     private bool initialized;
 
     // Start is called before the first frame update
@@ -22,8 +22,8 @@ public class AsyncParentPositioner : MonoBehaviour
         initialized = true;
         messagesList = GetComponentsInChildren<message>();
         numOfChilds = transform.childCount;
-        Transform trans = messagesList[0].transform;
-        newPosition = new Vector3(trans.position.x + 100, trans.position.y, trans.position.z);
+        // Transform trans = messagesList[0].transform;
+        // newPosition = new Vector3(trans.position.x, trans.position.y, trans.position.z);
     }
 
     // Update is called once per frame
@@ -39,7 +39,9 @@ public class AsyncParentPositioner : MonoBehaviour
         }
         foreach (message m in messagesList)
         {
-            m.transform.position = newPosition;
+            // m.transform.position = newPosition;
+            
+            m.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         }
     }
 }
--- Assets/Scripts/ScreenshotScript.cs
+++ Assets/Scripts/ScreenshotScript.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ScreenshotScript : MonoBehaviour
@@ -9,12 +10,28 @@ public class ScreenshotScript : MonoBehaviour
     public Button yourButton;
     public string fileName;
 
+    private Transform canvas;
+    private Transform camera;
+    private Camera camComponent;
+    
     void Start () {
 		Button btn = yourButton.GetComponent<Button>();
 		btn.onClick.AddListener(TaskOnClick);
-	}
+
+		canvas = GameObject.Find("Canvas").transform;
+		camera = transform;
+		camComponent = transform.GetComponent<Camera>();
+    }
 
 	void TaskOnClick(){
 		ScreenCapture.CaptureScreenshot($"Screenshots/{fileName}_{DateTime.Now:yyyyMMdd_HHmmss}.png");
+	}
+
+	void Update()
+	{
+		var pos = canvas.position;
+		pos.z = -1;
+		camera.position = pos;
+		camComponent.orthographicSize = pos.y;
 	}
 }
